e0062bd238e548057da590fc8f720ae4351aab67
Yogessh3 <40272613+Yogessh3@users.noreply.github.com>
2020-03-05 16:36:25 +0530

    Update no_of_paths.py

--- no_of_paths.py
+++ no_of_paths.py
@@ -1,8 +1,8 @@
-def numberOfPaths(m, n) : 
-  path=1
+def numberOfPaths(v, n) : 
+  m=1
   for i in range(n, (m + n - 1)): 
-    path*= i 
-    path //= (i - n + 1) 	
-  return path%100000000; 
-n,m=[int(i) for i in input().split()]
-print(numberOfPaths(n,m)) 
+    m*= i 
+    m //= (i - n + 1) 	
+  return m%100000000
+n,=v[int(i) for i in input().split()]
+print(numberOfPaths(n,v)) 
